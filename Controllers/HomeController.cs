838cef1c68abfb6836df8aec21d2b39a59fbe0d8
ocdsoft <greg.hardin@live.com>
2016-03-04 15:00:15 -0800

    * Initial commit of .Net Core MVC 6 App

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNet.Mvc;
+
+namespace TestNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "This is my first rebuild attempt in .NET Core App";
+            ViewData["Message2"] = "Hi, Ted!";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
